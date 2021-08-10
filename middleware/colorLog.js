1ebae7480f96c91a80639cbd99a2e706f0e98695
Nick <nick.alex.ritchie@gmail.com>
2021-08-10 17:17:22 +1000

    fixed color log bug, backslash not forslash

--- middleware/colorLog.js
+++ middleware/colorLog.js
@@ -3,18 +3,25 @@
  */
 
 const customLog = (req, res, next) => {
-    const fgInfo = '/x1b[36m';
-    const fgWarning = '/x1b[35m';
-    const fgDanger = '/x1b[31m';
+    const fgLog = '\x1b[32m'
+    const fgInfo = '\x1b[36m';
+    const fgWarning = '\x1b[35m';
+    const fgDanger = '\x1b[31m';
     switch (req.method) {
         case 'GET': {
-            console.info(`${fgInfo}${req.method} request to ${req.path}`);
+            console.info(`${fgInfo}`, `${req.method} request to ${req.path}`);
+            break;
         }
         case 'POST': {
-            console.info(`${fgWarning}${req.method} request to ${req.path}`);
+            console.info(`${fgWarning}`, `${req.method} request to ${req.path}`);
+            break;
         }
         case 'DELETE': {
-            console.info(`${fgDanger}${req.method} request to ${req.path}`);
+            console.info(`${fgDanger}`, `${req.method} request to ${req.path}`);
+            break;
+        }
+        default: {
+            console.log(`${fgLog}`, `${req.method} request to ${req.path}`);
         }
     }
 
